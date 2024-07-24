3168ead9dc016a4e54cf89becdfa1e79973b1d4d
Tania Zhuchenko <taniyhamail@gmail.com>
2024-07-24 18:37:36 +0300

    feat: added "task-completion" functionality

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,12 +1,26 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
+import { DUMMY_TASKS } from '../../dummy-tasks';
+import { Task } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input() name?: string;
+  dummyTasks = DUMMY_TASKS;
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name!: string;
+  completedTaskId?: string;
+
+  get selectedUserTasks() {
+    return this.dummyTasks.filter((task) => task.userId === this.userId);
+  }
+
+  onCompleteTask(id: string) {
+    this.dummyTasks = this.dummyTasks.filter((task) => task.id !== id);
+  }
 }
