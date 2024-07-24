b041a5dd4bb6d361ac7782320a4d9dcd48728b0f
Tania Zhuchenko <taniyhamail@gmail.com>
2024-07-24 13:21:27 +0300

    feat: added dynamic rendering and conditional rendering(Angular 17)

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,11 @@
 import { Component, Input, Output, EventEmitter, output } from '@angular/core';
 
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -7,18 +13,15 @@ import { Component, Input, Output, EventEmitter, output } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: User;
+
   @Output() select = new EventEmitter();
 
-  // select = output<string>();
-
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
